d96da9cdd877920069f81467f4427a8552f974e0
Stephen Maher <srqcoder@gmail.com>
2019-02-14 10:15:13 -0500

    Books LIst View in UI

---
+++ Services/BookServices/BooksListCombinedDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.BookServices
+{
+    public class BooksListCombinedDTO
+    {
+        public BooksListCombinedDTO(IEnumerable<BookListDTO> booksList)
+        {
+            BooksList = booksList;
+        }
+
+        public IEnumerable<BookListDTO> BooksList { get; private set; }
+    }
+}
