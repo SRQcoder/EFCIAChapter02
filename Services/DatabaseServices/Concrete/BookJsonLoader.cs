3af613fce20d5c92f125ff3120453d2d0a51b8f4
Stephen Maher <srqcoder@gmail.com>
2019-02-14 10:52:26 -0500

    Seeding pathway

---
+++ Services/DatabaseServices/Concrete/BookJsonLoader.cs
@@ -0,0 +1,67 @@
+﻿using DataLayer.EfCLasses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Services.DatabaseServices.Concrete
+{
+    public static class BookJsonLoader
+    {
+        public static IEnumerable<Book> LoadBooks(string fileDirectory, string fileSearchString)
+        {
+            var filePath = GetJsonFilePath(fileDirectory, fileSearchString);
+            var jsonDecoded = JsonConvert.DeserializeObject<ICollection<BookInfoJson>>(File.ReadAllText(filePath));
+
+            var authorDictionary = new Dictionary<string, Author>();
+            foreach (var bookInfoJson in jsonDecoded)
+            {
+                foreach (var author in bookInfoJson.authors)
+                {
+                    if (!authorDictionary.ContainsKey(author))
+                    {
+                        authorDictionary[author] = new Author { Name = author };
+                    }
+                }
+            }
+
+            return jsonDecoded.Select(x => CreateBookWithRefs(x, authorDictionary));
+        }
+
+
+        //  Private Methods
+        private static string GetJsonFilePath(string fileDirectory, string searchPattern)
+        {
+            var fileList = Directory.GetFiles(fileDirectory, searchPattern);
+            if (fileList.Length == 0)
+            {
+                throw new FileNotFoundException($"Could not find a file with the search name of {searchPattern} in directory {fileDirectory}");
+            }
+
+            return fileList.ToList().OrderBy(x => x).Last();
+        }
+
+        private static Book CreateBookWithRefs(BookInfoJson bookInfoJson, Dictionary<string, Author>  authorDictionary)
+        {
+            var book = new Book
+            {
+                Title = bookInfoJson.title,
+                Description = bookInfoJson.description,
+                PublishedOn = DecodePublishDate(bookInfoJson.publishedDate),
+            }
+        }
+
+        private static DateTime DecodePublishDate(string publishDate)
+        {
+            var split = publishDate.Split('-');
+            switch (split.Length)
+            {
+                case 1: return new DateTime(int.Parse(split[0]), 1, 1);
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Services/DatabaseServices/Concrete/SetupHelper.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Services.DatabaseServices.Concrete
+{
+    public enum DbStartupModes { UseExisting, EnsureCreated, EnsureDeletedCreated, UseMigrations }
+
+    public static class SetupHelper
+    {
+        private const string SeedDataSearchName = "apressBooks.json";
+        public const string SeedFileSubDirectory = "seedData";
+
+        public static void SeedDatabase(this EFCoreContext context, string dataDictionary)
+        {
+            if (!(context.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
+            {
+                var numbooks = context.Books.Count();
+                if (numbooks == 0)
+                {
+                    var books = BookJsonLoader.LoadBooks(Path.Combine(dataDictionary, SeedFileSubDirectory), SeedDataSearchName).ToList();
+                }
+            }
+        }
+    }
+}
