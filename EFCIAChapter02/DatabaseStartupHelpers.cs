a7b66ff275bda67e91dfcb95c44e7928359aa91d
Stephen Maher <srqcoder@gmail.com>
2019-02-15 10:59:19 -0500

    Updates to Database Startup

---
+++ EFCIAChapter02/DatabaseStartupHelpers.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer.EfCode;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Services.DatabaseServices.Concrete;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCIAChapter02
+{
+    public static class DatabaseStartupHelpers
+    {
+        private const string WwwRootDirectory = "wwwroot\\";
+        private const string FileToRead = @"C:\Users\Stephen\Source\Repos\EFCIAChapter02\EFCIAChapter02\wwwroot\";
+
+        public static string GetWwwRootPath()
+        {
+            //return Path.Combine(Directory.GetCurrentDirectory(), WwwRootDirectory);
+            return FileToRead;
+        }
+
+        public static IWebHost SetupDevelopmentDatabase(this IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                using (var context = services.GetRequiredService<EFCoreContext>())
+                {
+                    try
+                    {
+                        context.SeedDatabase(GetWwwRootPath());
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw ex;
+                    }
+                }
+            }
+
+            return webHost;
+        }
+    }
+}
--- EFCIAChapter02/Program.cs
+++ EFCIAChapter02/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.EfCode;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace EFCIAChapter02
@@ -14,7 +16,10 @@ namespace EFCIAChapter02
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .SetupDevelopmentDatabase()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
